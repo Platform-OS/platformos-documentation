bf7b0f08f216e0497674b42c89c2d5e3fb3711fc
Paweł Kowalski <pavelloz@gmail.com>
2017-11-06 18:06:25 +0100

    Add anchorjs to allow automatic deep linking

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 import '../scss/app.scss';
 
 import './modules/navigation';
+
+import anchorJS from 'anchor-js';
+const anchors = new anchorJS();
+anchors.options = {
+  placement: 'left',
+  icon: '#'
+};
+anchors.add('main h2, main h3');
