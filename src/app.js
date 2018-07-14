687a7ac43119712c58a699db1b259f620fb28faa
Paweł Kowalski <pavelloz@gmail.com>
2018-07-14 20:20:15 +0200

    Cleanup fonts, remove favicon link temp.

--- src/app.js
+++ src/app.js
@@ -5,10 +5,19 @@ import widgetNav from "./js/widgetNav";
 import tables from "./js/tables";
 import tabs from "./js/tabs";
 import scrollTo from "./js/scrollTo";
-
+import WebFont from "webfontloader";
 
 scrollTo();
 tabs();
 tables();
 widgetNav();
 btnMenu();
+
+WebFont.load({
+  google: {
+    families: ["Roboto:300,500,700"],
+    text:
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?.;,-_+={}[]"><@#~`$%^&*()'
+  },
+  timeout: 2000 // Set the timeout to two seconds
+});
