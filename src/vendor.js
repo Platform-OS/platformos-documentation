e7f414e3e9ab2f0990d48285ef3b7ce26f9d0d85
Paweł Kowalski <pavelloz@gmail.com>
2018-07-19 16:53:42 +0200

    Part 3 of housekeeping prism languages

--- src/vendor.js
+++ src/vendor.js
@@ -12,22 +12,21 @@ import cleanupHtml from "./js/cleanupHtml";
 // library.add(faExclamationTriangle, faCheck, faChevronRight, faSearch);
 
 if ($("[class^=language]").length > 0) {
-  const liquidKeywords = /\b(?:comment|endcomment|if|elsif|else|endif|unless|endunless|for|endfor|case|endcase|when|in|break|assign|continue|limit|offset|range|reversed|raw|endraw|capture|endcapture|tablerow|endtablerow|include|form|endform|render_form|query_graph|execute_query|content_for|endcontent_for|yield|input)\b/;
+  const liquidKeywords = /\b(?:comment|endcomment|if|elsif|else|endif|unless|endunless|for|endfor|case|endcase|when|in|break|assign|continue|limit|offset|range|reversed|raw|endraw|capture|endcapture|tablerow|endtablerow|include|form|endform|render_form|query_graph|execute_query|content_for|endcontent_for|yield|input|include_form|endinclude_form)\b/;
 
   [...document.querySelectorAll("code[class*=language]")].map(cleanupHtml);
 
   import(/* webpackChunkName: "prism" */ "prismjs");
-  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-ruby");
-  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-scss");
-  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-sass");
-  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-markdown");
-  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-haml");
-  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-css");
+  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-bash");
   import(/* webpackChunkName: "prism" */ "prismjs/components/prism-css-extras");
-  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-json");
-  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-javascript");
-  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-yaml");
+  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-css");
   import(/* webpackChunkName: "prism" */ "prismjs/components/prism-graphql");
+  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-javascript");
+  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-json");
+  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-markdown");
+  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-sass");
+  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-scss");
+  import(/* webpackChunkName: "prism" */ "prismjs/components/prism-yaml");
   import(/* webpackChunkName: "prism" */ "prismjs/components/prism-liquid").then(
     () => (Prism.languages.liquid.keyword = liquidKeywords) // Add our custom tags to liquid language definition
   );
