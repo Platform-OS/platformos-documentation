1342e2e30ceb8ea87811903ec54c47182ee557fd
Paweł Kowalski <pavelloz@gmail.com>
2018-07-19 10:43:59 +0200

    Improve code highlighting

--- src/js/cleanupHtml.js
+++ src/js/cleanupHtml.js
@@ -1,7 +1,7 @@
 export default function(element) {
   return (element.innerHTML = element.innerHTML
     .replace(/<p><\/p>/g, "") // empty markdown p's
-    .replace(/&/g, "&amp;") // TODO: Improve regexp to not touch yml/liquid comparisons - or - replace only in html tags.
+    // .replace(/&/g, "&amp;") // Turned off because it disables the possibility to use '&' in the code snippets
     .replace(/</g, "&lt;") // TODO: Improve regexp to not touch yml/liquid comparisons - or - replace only in html tags.
     .replace(/>/g, "&gt;") // TODO: Improve regexp to not touch yml/liquid comparisons - or - replace only in html tags.
     .replace(/"/g, "&quot;")
