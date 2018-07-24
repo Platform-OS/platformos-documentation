4f8c6834f9aeabbc60d3d3290b36da3e9bdfe73c
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2018-07-24 14:09:56 +0200

    Add log tag to docs

--- scripts/generate_liquid_tags.sh
+++ scripts/generate_liquid_tags.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "$0")"
 
 rm -rf ../tmp/liquid_tags/
-cd ../../desksnearme; yard -p ../nearme-documentation/generators/liquid_tags/ -o ../nearme-documentation/tmp/liquid_tags/ --no-progress --markup=markdown --tag arguments app/liquid_tags/*_tag.rb
-cd ../nearme-documentation; cp tmp/liquid_tags/*Tag.html marketplace_builder/views/partials/generated/liquid_tags/
+cd ../../desksnearme; yard -p ../nearme-documentation/generators/liquid_tags/ -o ../nearme-documentation/tmp/liquid_tags/ --no-progress --markup=markdown --tag arguments lib/liquify/tags/*_tag.rb
+cd ../nearme-documentation; cp tmp/liquid_tags/Liquify/Tags/*Tag.html marketplace_builder/views/partials/generated/liquid_tags/
