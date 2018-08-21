2fd806b7a8ac4d466b413b585dbe2fb78b7dabd9
Paweł Kowalski <pavelloz@gmail.com>
2018-08-21 16:12:58 +0200

    Scroll to content on page load

--- src/js/scrollToContent.js
+++ src/js/scrollToContent.js
@@ -1,8 +1,12 @@
-document.addEventListener("turbolinks:load", () => {
+const scrollToContent = () => {
   const viewportSize = document.body.dataset.viewportSize;
   const content = document.querySelector("section.content");
 
   if (content.scrollIntoView && (viewportSize === "xs" || viewportSize === "sm")) {
     content.scrollIntoView();
   }
-});
+};
+
+document.addEventListener("turbolinks:load", scrollToContent);
+
+scrollToContent();
