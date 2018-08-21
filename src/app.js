d83790b0efd19f3d81404d7e66945dc4cfe5983c
Paweł Kowalski <pavelloz@gmail.com>
2018-08-21 17:35:01 +0200

    Add deep links generation

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import "./js/syntaxHighlighting";
 import "./js/githubContributors";
 import "./js/externalLinks";
 import "./js/viewportClasses";
+import "./js/deepLinks";
 import "./js/scrollToContent";
 
 Turbolinks.start();
