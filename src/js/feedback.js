1908c95d3450687daf5087636232c2ce61759740
Paweł Kowalski <pavelloz@gmail.com>
2018-08-28 11:34:30 +0200

    WIP

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,46 +1,61 @@
-const feedbackSection = [...document.querySelectorAll('.feedback-form')];
+const form = document.querySelector('[data-feedback="form"]');
+const selectedValues = document.querySelectorAll('[data-feedback-selected-value]');
+const questionsContainer = document.querySelector('[data-feedback="questions"]');
+const questionValues = questionsContainer.querySelectorAll('[data-feedback-value]');
 
-const removeSelectedFeedback = () => {
+const showQuestionsContainer = () => questionsContainer.classList.remove('hidden');
+const showQuestion = value => form.querySelector(`[data-feedback-value="${value}"]`).classList.remove('hidden');
+const hideQuestions = () => [...questionValues].map(el => el.classList.add('hidden'));
 
-  const selectedFeedback = document.querySelectorAll('.feedback-selected');
-
-  if (selectedFeedback.length > 0 ) {
-    Array.from(selectedFeedback).forEach(el => {
-      el.classList.remove('feedback-selected');
-    });
-  }
-}
-
-const hideMessageLabels = () => {
-
-  var messageLabels = document.querySelectorAll('[data-feedback]');
-
-  if (messageLabels.length > 0 ) {
-    Array.from(messageLabels).forEach(el => {
-      el.classList.add('hidden');
-    });
-  }
-}
-
-const getFeedback = () => {
-  const feedbackElements = document.querySelectorAll('[data-feedback-rate]');
-  const feedbackQuestions = document.querySelector('[data-feedback-questions]');
-  const feedbackInput = document.querySelector('[data-feedback-input]');
-
-  Array.from(feedbackElements).forEach(el => {
-    el.addEventListener('click', function(event) {
-      removeSelectedFeedback();
-      hideMessageLabels();
-      feedbackQuestions.classList.remove('hidden');
-      el.querySelector('svg').classList.add('feedback-selected');
-      feedbackInput.value = el.dataset.feedbackRate;
-
-      document.querySelector('[data-feedback="' + el.dataset.feedbackRate +'"]').classList.remove("hidden");
+const attachEventHandlers = () => {
+  [...selectedValues].map(el => {
+    el.addEventListener("change", event => {
+      const selectedValue = event.target.value;
+      hideQuestions();
+      showQuestionsContainer();
+      showQuestion(selectedValue);
     });
   });
 }
 
-if (feedbackSection.length) {
-  getFeedback();
-}
+attachEventHandlers();
 
+// const removeSelectedFeedback = () => {
+//   const selectedFeedback = [...document.querySelectorAll(".feedback-selected")];
+
+//   if (selectedFeedback.length > 0) {
+//     selectedFeedback.forEach(el => {
+//       el.classList.remove("feedback-selected");
+//     });
+//   }
+// };
+
+// const hideMessageLabels = () => {
+//   var messageLabels = document.querySelectorAll("[data-feedback]");
+
+//   if (messageLabels.length > 0) {
+//     Array.from(messageLabels).forEach(el => {
+//       el.classList.add("hidden");
+//     });
+//   }
+// };
+
+// const getFeedback = () => {
+//   feedbackElements.forEach(el => {
+//     el.addEventListener("click", () => {
+//       removeSelectedFeedback();
+//       hideMessageLabels();
+//       feedbackQuestions.classList.remove("hidden");
+//       el.querySelector("svg").classList.add("feedback-selected");
+//       feedbackInput.value = el.dataset.feedbackRate;
+
+//       document
+//         .querySelector(`[data-feedback="${el.dataset.feedbackRate}"]`)
+//         .classList.remove("hidden");
+//     });
+//   });
+// };
+
+// if (feedbackSection.length > 0) {
+//   getFeedback();
+// }
