e710bc2fc62028baf4baf6f4caa3d56a88f03f23
Paweł Kowalski <pavelloz@gmail.com>
2018-08-31 15:53:47 +0200

    2nd part of fixing turbolinks vs feedback

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,28 +1,31 @@
-const form = document.querySelector('[data-feedback="form"]');
-const selectedValues = document.querySelectorAll('[data-feedback-selected-value]');
-const questionsContainer = document.querySelector('[data-feedback="questions"]');
-const questionValues = document.querySelectorAll('[data-feedback-value]');
+const form = () => document.querySelector('[data-feedback="form"]');
+const selectedValues = () => document.querySelectorAll("[data-feedback-selected-value]");
+const questionsContainer = () => document.querySelector('[data-feedback="questions"]');
+const questionValues = () => document.querySelectorAll("[data-feedback-value]");
 
-const toggleQuestionsContainer = addOrRemove => questionsContainer.classList.toggle('hidden', addOrRemove);
-const hideQuestions = () => [...questionValues].map(el => el.classList.add('hidden'));
-const showQuestion = value => form.querySelector(`[data-feedback-value="${value}"]`).classList.remove('hidden');
+const toggleQuestionsContainer = addOrRemove => questionsContainer().classList.toggle("hidden", addOrRemove);
+const hideQuestions = () => [...questionValues()].map(el => el.classList.add("hidden"));
+const showQuestion = value =>
+  form()
+    .querySelector(`[data-feedback-value="${value}"]`)
+    .classList.remove("hidden");
 
-const updateFormForCustomizationUpdate = (id) => {
-  form.setAttribute('action', `/api/customizations/${id}`);
-  form.insertAdjacentHTML('beforeend', '<input type="hidden" name="_method" value="PUT">');
-}
+const updateFormForCustomizationUpdate = id => {
+  form().setAttribute("action", `/api/customizations/${id}`);
+  form().insertAdjacentHTML("beforeend", '<input type="hidden" name="_method" value="PUT">');
+};
 
 const sendFeedback = () => {
-  return fetch(form.getAttribute('action'), {
-    method: form.getAttribute('method'),
-    body: new FormData(form)
+  return fetch(form().getAttribute("action"), {
+    method: form().getAttribute("method"),
+    body: new FormData(form())
   })
-  .then(res => res.json())
-  .then(res => updateFormForCustomizationUpdate(res.id))
-  .catch(e => {
-    Raven.captureException(e);
-  });
-}
+    .then(res => res.json())
+    .then(res => updateFormForCustomizationUpdate(res.id))
+    .catch(e => {
+      throw new Error(e);
+    });
+};
 
 const onRatingSelected = event => {
   const selectedValue = event.target.value;
@@ -36,8 +39,7 @@ const onRatingSelected = event => {
 };
 
 document.addEventListener("turbolinks:load", () => {
-  if (form) {
-    [...selectedValues].map(el => el.addEventListener("change", onRatingSelected));
+  if (form()) {
+    [...selectedValues()].map(el => el.addEventListener("change", onRatingSelected));
   }
 });
-
