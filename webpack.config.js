19398f83f89d7a5815d25493891d2414bf024882
Paweł Kowalski <pavelloz@gmail.com>
2018-10-15 16:48:43 +0200

    Update webpack config to include sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,71 @@
 const path = require("path");
+const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WebpackRequireFrom = require("webpack-require-from");
 
 const BUILD_DIR = path.join(__dirname, process.env.npm_package_config_build_dir);
 
-const isProduction = process.env.NODE_ENV === "production";
+const production = process.env.NODE_ENV === "production";
 
-module.exports = () => {
-  return {
-    entry: {
-      app: "./src/app",
-      webfonts: "./src/webfonts"
-    },
-    output: {
-      filename: "[name].js",
-      chunkFilename: "[name].[chunkhash:3].js",
-      publicPath: "",
-      path: BUILD_DIR
-    },
-    devtool: false,
-    bail: true,
-    stats: {
-      modules: false,
-      hash: false,
-      assetsSort: "!size",
-      children: false
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: "babel-loader?retainLines=true&cacheDirectory"
-        },
-        {
-          test: /(\.css|\.scss)$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader?url=false", "postcss-loader", "sass-loader"]
-        },
-        {
-          test: /\.gif$/,
-          use: ["file-loader?name=[name].gif"]
-        },
-        {
-          test: /\.(eot|ttf|otf|svg|woff(2))?$/,
-          loader: "file-loader?name=fonts/[name].[ext]"
-        }
-      ]
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: "[name].css",
-        chunkFilename: "[name].css"
-      }),
-      new WebpackRequireFrom({
-        methodName: "__cdnUrl"
-      })
-    ],
-    mode: isProduction ? "production" : "development"
-  };
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: "[name].css",
+    chunkFilename: "[name].css"
+  }),
+  new WebpackRequireFrom({
+    methodName: "__cdnUrl"
+  })
+];
+
+if (production) {
+  plugins.push(
+    new webpack.SourceMapDevToolPlugin({
+      filename: "[file].map",
+      include: ["app.js"]
+    })
+  );
+}
+
+const config = {
+  entry: {
+    app: "./src/app",
+    webfonts: "./src/webfonts"
+  },
+  output: {
+    filename: "[name].js",
+    chunkFilename: "[name].[chunkhash:3].js",
+    publicPath: "",
+    path: BUILD_DIR
+  },
+  bail: true,
+  stats: {
+    modules: false,
+    hash: false,
+    assetsSort: "!size",
+    children: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader?retainLines=true&cacheDirectory"
+      },
+      {
+        test: /(\.css|\.scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader?url=false", "postcss-loader", "sass-loader"]
+      },
+      {
+        test: /\.gif$/,
+        use: ["file-loader?name=[name].gif"]
+      },
+      {
+        test: /\.(eot|ttf|otf|svg|woff(2))?$/,
+        loader: "file-loader?name=fonts/[name].[ext]"
+      }
+    ]
+  },
+  plugins: plugins,
+  mode: production ? "production" : "development"
 };
+
+module.exports = config;
