c96868726419cc8d0ea73b208477ea4690075a95
Paweł Kowalski <pavelloz@gmail.com>
2018-10-31 20:34:55 +0100

    Reword test [ci skip]

--- tests/e2e/Search.js
+++ tests/e2e/Search.js
@@ -8,16 +8,16 @@ const Search = new SearchPO();
 
 fixture("Search").page(Layout.URL.staging);
 
-test("Results contain phrase when exists", async t => {
-  await Search.go(Search.existingPhrase);
-  await t.expect(Selector("h2").withText(Search.existingPhrase).count).eql(1);
-});
-
 test("There are no liquid errors on the page", async t => {
   await Search.go(Search.existingPhrase);
   await t.expect(Layout.Body.withText("Liquid Error").count).eql(0);
   await Search.go(Search.nonExistingPhrase);
   await t.expect(Layout.Body.withText("Liquid Error").count).eql(0);
+});
+
+test("Results contain phrase when exists", async t => {
+  await Search.go(Search.existingPhrase);
+  await t.expect(Selector("h2").withText(Search.existingPhrase).count).eql(1);
 });
 
 test("Pagination is working", async t => {
@@ -34,18 +34,18 @@ test("There is no pagination if there is no results", async t => {
   await t.expect(Search.Pagination.count).eql(0);
 });
 
-test("Search icon is submitting the form", async t => {
+test("Clicking on search icon is submitting the form", async t => {
   await t.click(Search.SubmitButton);
   const pathname = await t.eval(() => window.location.pathname);
   await t.expect(pathname === "/search").ok();
 });
 
-test("Results are empty for random string", async t => {
+test("There are no results for random string", async t => {
   await Search.go(Search.nonExistingPhrase);
   await t.expect(Search.ResultsItems.count).eql(0);
 });
 
-test("Results does contain pages that have searchable: false", async t => {
+test("Results does not contain pages that have are not searchable", async t => {
   await Search.go(Search.nonSearchablePhrase);
   await t.expect(Search.ResultsItems.count).eql(0);
 });
