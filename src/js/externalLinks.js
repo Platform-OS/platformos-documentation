548132abe60faed502d0905f50f8b91b95c65833
Paweł Kowalski <pavelloz@gmail.com>
2018-11-01 16:56:56 +0100

    Fix external links attributes on turbolinks load

--- src/js/externalLinks.js
+++ src/js/externalLinks.js
@@ -1,8 +1,8 @@
-const externalLinks = [...document.querySelectorAll('a[href^="http"]')];
+const externalLinks = () => [...document.querySelectorAll('a[href^="http"]')];
 
 document.addEventListener("turbolinks:load", () => {
-  if (externalLinks.length) {
-    externalLinks.map(link => {
+  if (externalLinks().length) {
+    externalLinks().map(link => {
       link.setAttribute("target", "_blank");
       link.setAttribute("rel", "external noopener");
     });
