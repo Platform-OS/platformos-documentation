56fb274b70f0ae6098cd80ad5392c73873702be8
Paweł Kowalski <pavelloz@gmail.com>
2018-11-02 01:22:40 +0100

    Turn off concurrency for CI as it doesnt perform

---
+++ tests/e2e.js
@@ -0,0 +1,20 @@
+const createTestCafe = require("testcafe");
+
+let testcafe = null;
+let runner = null;
+
+createTestCafe("localhost", 1337, 1338)
+  .then(tc => {
+    testcafe = tc;
+    runner = testcafe.createRunner();
+
+    return runner
+      .src(["tests/e2e/Layout.js", "tests/e2e/Homepage.js", "tests/e2e/Search.js"])
+      .browsers(["chrome:headless"])
+      .reporter("list")
+      .run();
+  })
+  .then(failedCount => {
+    testcafe.close();
+    failedCount === 0 ? process.exit(0) : process.exit(1);
+  });
