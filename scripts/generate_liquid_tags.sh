e76ab4292171ab0fef5637d7e6066ca9cba782ff
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2018-11-28 09:05:16 +0100

    Fix generator for new directory structure in europe branch

--- scripts/generate_liquid_tags.sh
+++ scripts/generate_liquid_tags.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 
 rm -rf ../tmp/liquid_tags/
 
-cd ../../desksnearme; yard -p ../nearme-documentation/generators/liquid_tags/ -o ../nearme-documentation/tmp/liquid_tags/ --no-progress --markup none --tag arguments lib/liquify/tags/*_tag.rb
+cd ../../desksnearme; yard -p ../nearme-documentation/generators/liquid_tags/ -o ../nearme-documentation/tmp/liquid_tags/ --no-progress --markup none --tag arguments app/lib/liquify/tags/*_tag.rb
 cd ../nearme-documentation; cp tmp/liquid_tags/Liquify/Tags/*Tag.html marketplace_builder/views/partials/generated/liquid_tags/
