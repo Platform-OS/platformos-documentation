b6a80db5e88e0aa2f14f7b50807f3787af70adc6
Paweł Kowalski <pavelloz@Gmail.com>
2019-02-12 19:56:12 +0100

    Toc (#400)
    * Implement MVP of ToC
    
    * Autosteps generation
    
    * Style autosteps
    
    * Use autosteps in all pages that have steps
    
    * Add test for autosteps
    
    * Enable toc for reference api
    
    * Make non-code headings h4 in api ref

--- src/js/helpers/headings.js
+++ src/js/helpers/headings.js
@@ -1,11 +1,10 @@
-const getHeadings = () => [...document.querySelectorAll('.content h2[id], .content h3[id]')] || [];
+const getHeadings = () => [...document.querySelectorAll('.content h2[id]')] || [];
 
 const parseHeadings = headings => {
   return headings.map(heading => {
     return {
       href: `#${heading.id}`,
-      text: heading.textContent,
-      type: heading.tagName.toLowerCase()
+      text: heading.textContent
     };
   });
 };
