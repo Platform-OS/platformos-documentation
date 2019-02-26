9922b634ff4c549633f4348fdc8f264863a31937
Paweł Kowalski <pavelloz@gmail.com>
2019-02-26 22:09:36 +0100

    Refactor + homepage fix

--- src/js/sidebarMenu.js
+++ src/js/sidebarMenu.js
@@ -3,20 +3,16 @@ import getParentsUntil from './helpers/getParentsUntil';
 
 const getActiveLink = ({ menu, path }) => menu.querySelector(`a[href="${path}"]`);
 
-const markActiveLink = ({ menu, path }) => {
-  const link = getActiveLink({ menu, path });
-
-  link.closest('li').classList.add('last-level-nested', 'active');
+const markActiveLink = activeLink => {
+  activeLink.closest('li').classList.add('last-level-nested', 'active');
 };
 
-const markActiveParents = ({ menu, path }) => {
-  const link = getActiveLink({ menu, path });
-
-  getParentsUntil(link, '.sidebar-menu')
+const markActiveParents = activeLink => {
+  getParentsUntil(activeLink, '.sidebar-menu')
     .filter(el => el.matches('.sub-menu'))
     .map(el => el.classList.add('in'));
 
-  getParentsUntil(link, '.sidebar-menu')
+  getParentsUntil(activeLink, '.sidebar-menu')
     .filter(el => el.matches('.has-submenu'))
     .map(el => el.classList.add('active'));
 };
@@ -24,14 +20,16 @@ const markActiveParents = ({ menu, path }) => {
 const initialize = () => {
   const menu = document.querySelector('.sidebar-menu');
   const path = location.pathname;
+  const activeLink = getActiveLink({ menu, path });
 
-  if (!menu) {
+  new MetisMenu('.sidebar-menu');
+
+  if (!menu || !activeLink) {
     return;
   }
 
-  markActiveParents({ menu, path });
-  markActiveLink({ menu, path });
-  new MetisMenu('.sidebar-menu');
+  markActiveParents(activeLink);
+  markActiveLink(activeLink);
 };
 
 document.addEventListener('turbolinks:load', initialize);
