f4cf065a7181b9850d02a0a43ddd19250b8f1c18
Paweł Kowalski <pavelloz@gmail.com>
2019-06-03 20:36:43 +0200

    Finish up gql

--- src/js/remote-docs/templates/index.js
+++ src/js/remote-docs/templates/index.js
@@ -1,12 +1,3 @@
 module.exports = {
-  portal: require('./portal'),
-  filters: require('./filters'),
-  tags: require('./tags'),
-  'graphql-queries': require('./graphql/queries'),
-  'graphql-mutations': require('./graphql/mutations'),
-  'graphql-objects': require('./graphql/objects'),
-  'graphql-scalars': require('./graphql/scalars'),
-  'graphql-interfaces': require('./graphql/interfaces'),
-  'graphql-enums': require('./graphql/enums'),
-  'graphql-inputs': require('./graphql/inputs')
+  portal: require('./portal')
 };
