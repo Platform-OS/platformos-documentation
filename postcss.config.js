6e44169b76a5aed79c6f94f5afb1ccc86eab0651
Paweł Kowalski <pavelloz@gmail.com>
2019-06-05 17:10:40 +0200

    Cleanup postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,8 @@ module.exports = () => {
   return {
     plugins: [
       require('css-mqpacker'),
-      require('postcss-focus'),
-      require('postcss-calc'),
-      require('postcss-pseudoelements'),
       require('autoprefixer'),
-      require('postcss-fixes'),
-      require('postcss-font-display')({ display: 'swap', replace: false })
+      require('postcss-fixes')
     ]
   };
 };
