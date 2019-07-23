67309302d9eee51f4e70021f2968fad6371a2bea
Paweł Kowalski <pavelloz@gmail.com>
2019-07-23 15:49:46 +0200

    Add new! to articles updated in last 7 days

--- src/js/sidebarMenu.js
+++ src/js/sidebarMenu.js
@@ -1,5 +1,20 @@
-import { $qa } from './helpers/dom';
+import { $qa, $q } from './helpers/dom';
 
 const currentPath = location.pathname;
 
+// Mark currently visited page
 $qa(`.nav-sidebar a[href="${currentPath}"]`).map(a => a.classList.add('active'));
+
+// Add "New!" to categories that have recently updated articles inside
+const firstLevel = $qa('.nav-sidebar > ul > li:not(.topic-group):not(.active)');
+
+for (let index in firstLevel) {
+  const item = firstLevel[index];
+  const newIndicator = $q('.new', item);
+  const hasNew = $q('.submenu', item) && !!newIndicator;
+
+  if (hasNew) {
+    $q('a', item).appendChild(newIndicator.cloneNode(true));
+    continue;
+  }
+}
