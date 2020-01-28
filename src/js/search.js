6a54adff659c7a150e59cee46b1c2be2a763c7c5
Paweł Kowalski <pavelloz@gmail.com>
2020-01-28 13:34:58 +0100

    Separate search css into its own file and lazyload it

--- src/js/search.js
+++ src/js/search.js
@@ -1,4 +1,6 @@
+import(/* webpackChunkName: "search" */ '../../node_modules/docsearch.js/dist/npm/styles/main.scss');
 import docsearch from 'docsearch.js';
+
 docsearch({
   apiKey: '76cf08e1e1a7015f417f8a3e86b70c8f',
   indexName: 'platformos',
