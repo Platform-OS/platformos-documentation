c07b45639b834a44edb497be0b8ad60b93736820
Pawel Kowalski <pavelloz@gmail.com>
2020-05-14 16:15:52 +0200

    Make ToC more compact

--- src/js/toc.js
+++ src/js/toc.js
@@ -8,7 +8,7 @@ const getContainer = () => $q('[data-autotoc]');
 const generateTOCList = (headings) => {
   return headings
     .map((h) => {
-      return `<li>
+      return `<li class="mb-0">
         <a href="${h.href}" class="no-underline hover:text-pos-darkblue inline-block py-1">
           ${h.text}
         </a>
@@ -29,7 +29,7 @@ const initialize = () => {
 
   const tocDOM = `<div class="hidden md:block p-8 bg-pos-page-bg">
       <h4 class="mb-4 text-lg">On this page</h4>
-      <ul class="list-none">${tocHTML}</ul>
+      <ul class="list-none pl-0">${tocHTML}</ul>
     </div>`;
 
   container.innerHTML = tocDOM;
