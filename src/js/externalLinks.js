f4f6e33c07952e8ba1ce07d27a0939fdc40d6938
Paweł Kowalski <pavelloz@gmail.com>
2020-06-15 10:54:04 +0200

    Add titles to SVGs, confirmation to external link

--- src/js/externalLinks.js
+++ src/js/externalLinks.js
@@ -3,8 +3,12 @@ import { $qa } from './helpers/dom';
 const externalLinks = () => $qa('a[href^="http"]');
 
 if (externalLinks().length > 0) {
-  externalLinks().map(link => {
+  externalLinks().map((link) => {
     link.setAttribute('target', '_blank');
     link.setAttribute('rel', 'external noopener');
+    link.onclick = () => {
+      const result = window.confirm('This link will open in new Tab.');
+      return result;
+    };
   });
 }
