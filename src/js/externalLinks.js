c7b3cd3df6db262b89a0c76c0dba2d60fb43ee17
Paweł Kowalski <pavelloz@gmail.com>
2020-06-15 11:09:06 +0200

    Refactor external link confirmation

--- src/js/externalLinks.js
+++ src/js/externalLinks.js
@@ -6,9 +6,6 @@ if (externalLinks().length > 0) {
   externalLinks().map((link) => {
     link.setAttribute('target', '_blank');
     link.setAttribute('rel', 'external noopener');
-    link.onclick = () => {
-      const result = window.confirm('This link will open in new Tab.');
-      return result;
-    };
+    link.setAttribute('onclick', 'return confirm("This link will open in new tab.")');
   });
 }
