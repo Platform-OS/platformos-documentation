41dbe3043a4a0f705229dba7496d5715b645b6f5
Paweł Kowalski <pavelloz@gmail.com>
2021-03-18 23:39:36 +0100

    Cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,39 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  // future: {
-  //   removeDeprecatedGapUtilities: true,
-  //   purgeLayersByDefault: true,
-  //   defaultLineHeights: true,
-  //   standardFontWeights: true
-  // },
-  purge: [
-    './app/forms/**/*.liquid',
-    './app/views/**/*.liquid',
-    './src/js/**/*.js',
-    './modules/graphql/**/*.mustache',
-  ],
+  purge: {
+    content: [
+      './app/forms/**/*.liquid',
+      './app/views/**/*.liquid',
+      './src/js/**/*.js',
+      './modules/graphql/template/*.mustache',
+    ],
+  },
   theme: {
-    colors: {
-      'white': '#ffffff',
-      'black': '#000000',
-      'body': '#4a4a4a',
-      pos: {
-        'blue': '#3A8DDE',
-        'darkblue': '#194F90',
-        'searchblue': '#2173C4',
-        'page-bg': '#F5F6FC',
-        'divider': '#dddddd',
-        'search': '#767676',
-        'green': '#45A041',
-        'tip-bg': '#EEEEF2',
-        'tip-text': '#6F7286',
-        'note-bg': '#E2EEFA',
-        'note-text': '#2E71B2',
-        'important-bg': '#FFF3E6',
-        'important-text': '#B35C00',
-        'warning-bg': '#FFE9EE',
-        'warning-text': '#AA002C',
-        'success-bg': '#E9F3E8',
-        'success-text': '#166D12',
-        'icon-feedback': '#8589A0'
-      },
-      'gray': {
-        200: '#eeeeee'
-      }
-    },
     extend: {
+      colors: {
+        'body': '#4a4a4a',
+        pos: {
+          'blue': '#3A8DDE',
+          'darkblue': '#194F90',
+          'searchblue': '#2173C4',
+          'page-bg': '#F5F6FC',
+          'divider': '#dddddd',
+          'search': '#767676',
+          'green': '#45A041',
+          'tip-bg': '#EEEEF2',
+          'tip-text': '#6F7286',
+          'note-bg': '#E2EEFA',
+          'note-text': '#2E71B2',
+          'important-bg': '#FFF3E6',
+          'important-text': '#B35C00',
+          'warning-bg': '#FFE9EE',
+          'warning-text': '#AA002C',
+          'success-bg': '#E9F3E8',
+          'success-text': '#166D12',
+          'icon-feedback': '#8589A0'
+        },
+      },
       boxShadow: {
         box: '0px 8px 18px rgba(52, 61, 75, 0.3)'
       },
