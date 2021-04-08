b34b81d0bf0afd37184dbc97d2b86d0b0ef4b50e
Paweł Kowalski <pavelloz@gmail.com>
2021-04-08 19:16:36 +0200

    Remove cssnano

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /(\.css)$/,
+        test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
           { loader: 'css-loader', options: { url: false } },
