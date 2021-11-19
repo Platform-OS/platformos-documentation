752be9e32e97f1078306b41e40badfa0d5873739
piotrze <piotr.bliszczyk@gmail.com>
2021-11-19 12:01:58 +0100

    New graphql api pages generator

--- scripts/generate-graphql.sh
+++ scripts/generate-graphql.sh
@@ -1,6 +1,10 @@
-npm i pos-graphdocs && \
-node node_modules/pos-graphdocs/bin/graphdoc.js --force \
-  --endpoint ${MPKIT_URL}/api/graph \
-  --template "modules/graphql/template" \
-  --extension html \
-  --header "Authorization: Token ${MPKIT_TOKEN}"
+npm i graphql@16.0.1 && \
+cp ./scripts/astFromValue.js ./node_modules/graphql/utilities/astFromValue.js
+npm i @platformos/graphql-docs-markdown && \
+curl ${MPKIT_URL}/api/graph/schema.graphql \
+node  node_modules/@platformos/graphql-docs-markdown/src/start.js graphql-to-doc \
+  --root="./modules/graphql/public/views/pages" \
+  --base="api-reference/graphql" \
+  --groupByDirective="@doc(category|=common)" \
+  --navigation="./app/views/partials/shared/nav/graphql-navigation.liquid" \
+  --schema="./schema.graphql"
