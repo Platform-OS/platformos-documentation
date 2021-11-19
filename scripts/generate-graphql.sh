e6ab4d0d8f8ed63e6dd0d82f0e9ae4897064c1d0
piotrze <piotr.bliszczyk@gmail.com>
2021-11-19 12:11:25 +0100

    Fix

--- scripts/generate-graphql.sh
+++ scripts/generate-graphql.sh
@@ -1,7 +1,8 @@
 npm i graphql@16.0.1 && \
-cp ./scripts/astFromValue.js ./node_modules/graphql/utilities/astFromValue.js
+cp ./scripts/astFromValue.js ./node_modules/graphql/utilities/astFromValue.js && \
 npm i @platformos/graphql-docs-markdown && \
-curl ${MPKIT_URL}/api/graph/schema.graphql \
+curl ${MPKIT_URL}/api/graph/schema.graphql > schema.graphql && \
+rm -rf /tmp/@edno/ && \
 node  node_modules/@platformos/graphql-docs-markdown/src/start.js graphql-to-doc \
   --root="./modules/graphql/public/views/pages" \
   --base="api-reference/graphql" \
