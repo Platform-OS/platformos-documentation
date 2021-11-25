a0b543395ff5fcc0fc02fb7b06726165b1e1e7ba
piotrze <piotr.bliszczyk@gmail.com>
2021-11-25 09:54:06 +0100

    Debug graphql build

--- scripts/generate-graphql.sh
+++ scripts/generate-graphql.sh
@@ -1,11 +1,8 @@
 npm i graphql@16.0.1 && \
 cp ./scripts/astFromValue.js ./node_modules/graphql/utilities/astFromValue.js && \
 npm i @platformos/graphql-docs-markdown && \
-echo ${MPKIT_URL:0:10} && \
-echo ${MPKIT_URL:10:30} && \
 VERSION_URL=${MPKIT_URL}/_version
-echo $VERSION_URL
-PLATFORM_VERSION=`curl $VERSION_URL` && \
+PLATFORM_VERSION=${curl $VERSION_URL} && \
 echo $PLATFORM_VERSION && \
 curl https://deidcfp1yn7c2.cloudfront.net/platform_docs/${PLATFORM_VERSION}/schema.graphql > schema.graphql && \
 rm -rf /tmp/@edno/ && \
