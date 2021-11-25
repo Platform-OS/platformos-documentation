fcd6f83f3ad695609936e62d3112f216fd7265de
piotrze <piotr.bliszczyk@gmail.com>
2021-11-25 10:11:30 +0100

    Remove debug echo

--- scripts/generate-graphql.sh
+++ scripts/generate-graphql.sh
@@ -3,7 +3,6 @@ cp ./scripts/astFromValue.js ./node_modules/graphql/utilities/astFromValue.js &&
 npm i @platformos/graphql-docs-markdown && \
 VERSION_URL=${MPKIT_URL}_version && \
 PLATFORM_VERSION=$(curl $VERSION_URL) && \
-echo $PLATFORM_VERSION && \
 curl https://deidcfp1yn7c2.cloudfront.net/platform_docs/${PLATFORM_VERSION}/schema.graphql > schema.graphql && \
 rm -rf /tmp/@edno/ && \
 node  node_modules/@platformos/graphql-docs-markdown/src/start.js graphql-to-doc \
