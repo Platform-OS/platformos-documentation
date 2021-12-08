6f73fb18c383c4022691fe4b5b3dd724c8b62484
Pawel Kowalski <pavelloz@gmail.com>
2021-12-08 13:23:04 +0100

    Add best onboarding award

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackRequireFrom = require('webpack-require-from');
 const webpack = require('webpack');
 const { ESBuildMinifyPlugin } = require('esbuild-loader');
+const BrotliPlugin = require('brotli-webpack-plugin');
 
 const production = process.env.NODE_ENV === 'production';
 
@@ -53,7 +54,13 @@ module.exports = {
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
       },
-    })
+    }),
+    new BrotliPlugin({
+      asset: '[path].br',
+      test: /\.(js|css)$/,
+      threshold: 10240,
+      minRatio: 0.8
+  })
   ],
   mode: production ? 'production' : 'development',
 };
