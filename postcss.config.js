48ce22f44319af387efd309a61c325a75a6906db
Pawel Kowalski <pavelloz@gmail.com>
2022-02-14 12:29:49 +0100

    Update npm dependencies. Remove postcss-fixes

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 module.exports = () => {
-  const prod = process.env.NODE_ENV === 'production';
 
   return {
     plugins: [
-      require('postcss-fixes')(),
       require('postcss-import')(),
       require('autoprefixer')(),
       require('tailwindcss')()
