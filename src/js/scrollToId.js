2ef467f27c004679ca188dcfa92bfa4ed2713c7d
Łukasz Krysiewicz <zx@secondgate.pl>
2022-10-24 15:32:24 +0200

    Updated deep linking not to use JS

--- src/js/scrollToId.js
+++
@@ -1,5 +0,0 @@
-import { $q } from './helpers/dom';
-
-if(document.location.hash){
-  $q(document.location.hash)?.scrollIntoView({ behavior: "smooth" });
-}
--- tests/codecept/Deeplinks_test.js
+++ tests/codecept/Deeplinks_test.js
@@ -1,15 +1,7 @@
 Feature('Deep links');
 
 Scenario('Deep linking to headers is working', async ({ I }) => {
-  I.amOnPage('/get-started/quickstart-guide');
+  I.amOnPage('/get-started');
 
-  I.seeElement('.content__main .anchorjs-element');
-});
-
-Scenario('Deep linking is working with utf8 characters in the heading id/href', async ({ I }) => {
-  I.amOnPage(
-    '/developer-guide/records/building-contact-form-with-records#step-2-define-contact-form-–-form-object'
-  );
-
-  I.seeElement('#step-2-define-contact-form-–-form-object');
+  I.seeElement('.content__main .permalink');
 });
