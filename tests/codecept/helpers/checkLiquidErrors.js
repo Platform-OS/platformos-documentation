307b10fa2c5af05eee6e36b1ce510e8252ace666
Filip Klosowski <filip@near-me.com>
2025-04-22 12:08:25 +0200

    Updated helper

--- tests/codecept/helpers/checkLiquidErrors.js
+++ tests/codecept/helpers/checkLiquidErrors.js
@@ -1,14 +1,10 @@
 const { I } = inject();
 
-module.exports = function() {
-  return actor({
-
-    checkLiquidErrors: function() {
-      I.dontSee('Liquid Error');
-      I.dontSee('RenderFormTag Error');
-      I.dontSee('QueryGraphTag Error');
-      I.dontSee('translation missing');
-    }
-
-  });
-}
+module.exports = {
+  checkLiquidErrors: function() {
+    I.dontSee('Liquid Error');
+    I.dontSee('RenderFormTag Error');
+    I.dontSee('QueryGraphTag Error');
+    I.dontSee('translation missing');
+  }
+};
